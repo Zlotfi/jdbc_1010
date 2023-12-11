28d93d7fec650596042552ad736d57bf8efb3389
victus <zlotfi758@gmail.com>
2023-12-11 18:24:49 +0330

    add getConnection method

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnection {
-    Connection connection = DriverManager.getConnection
+    public Connection connection = DriverManager.getConnection
             ("jdbc:postgresql://localhost:5432/postgres","postgres","zl13801393#");
 
     public JdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
