0e111a13057d6081a74193a68cf565df4d6a57a8
victus <zlotfi758@gmail.com>
2023-12-11 23:04:56 +0330

    add main method for test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import menu.Menu;
+import model.User;
+import repository.UserRepository;
+import service.UserService;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+//        UserService userService = new UserService();
+//        userService.register();
+//        userService.login();
+//        userService.changeFirstName();
+//        userService.delete();
+
+        Menu menu = new Menu();
+        menu.publicMenu();
+    }
 }
