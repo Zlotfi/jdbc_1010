e1ae329f21d02bbb013da88443c59dbf7a6295fb
victus <zlotfi758@gmail.com>
2023-12-11 20:02:53 +0330

    add login method

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,6 +4,7 @@ import model.User;
 import repository.UserRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class UserService {
 
@@ -19,5 +20,20 @@ public class UserService {
             System.out.println(user.getFirstName() + " successfully added to database");
         else
             System.out.println("OOps!:(");
+    }
+
+    public void login() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your userName: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("please enter your password: ");
+        String password = scanner.nextLine();
+
+        User user = userRepository.login(userName);
+        if ((user != null) && user.getPassword().equals(password))
+            System.out.println("login successfully");
+        else
+            System.out.println("bad credentials");
     }
 }
